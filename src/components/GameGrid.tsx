33565528958ab3429af23b269e421ebb190d1fa5
ck496 <chriskurian24@gmail.com>
2025-06-15 22:53:30 -0700

    refactor GameGrid component and moved games fetching logic to a new useGames hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useGames from "../hooks/useGames";
 import { Text } from "@chakra-ui/react";
 
-interface Game {
-  id: number;
-  name: string;
-}
-interface GetGamesResponse {
-  const: number;
-  results: Game[];
-}
-
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GetGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  // Get games and error from useGames hook
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+interface GetGamesResponse {
+  const: number;
+  results: Game[];
+}
+
+// Custom hook to fetch and return a list of games from the using an axios apiClient
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    // AbortController to handle cancellations if component unmounts while request is still processing
+    const controller = new AbortController();
+
+    apiClient
+      .get<GetGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        // ignore errors caused by Cancellations
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    // Cleanup function to cancel request when component unmounts
+    return () => controller.abort();
+  }, []); // Empty dependency array ([]) = run once on mount
+
+  return { games, error };
+};
+
+export default useGames;
