8f71684489daddceefe2c09f70626b85c52acbf1
ck496 <chriskurian24@gmail.com>
2025-06-15 18:48:03 -0700

    added responsive grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //For devices larger than 1020 px show side bar etc
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <Show above="lg"> 
+        {/* Only show Component for lg size displays and above */}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="coral">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
