be0e7313991aa345cb32f98537fdf6b6d23f9d81
ck496 <chriskurian24@gmail.com>
2025-06-20 16:45:34 -0700

    chore: clean up genre list side bar UI

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,13 @@
-
 import useGenres, { Genre } from "../hooks/useGenres";
-import { List, ListItem, Text, Image, HStack, Button } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  Text,
+  Image,
+  HStack,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import getCroppedUrl from "../services/image-urls";
 import GenreListSkeleton from "./GenreListSkeleton";
 
@@ -13,28 +20,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   if (error) return null;
   return (
-    <List>
-      {isLoading && <GenreListSkeleton />}
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedUrl(genre.image_background)}
-            />
-            <Button
-              variant="link"
-              fontSize="lg"
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {isLoading && <GenreListSkeleton />}
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedUrl(genre.image_background)}
+              />
+              <Button
+                variant="link"
+                fontSize="lg"
+                whiteSpace="normal"
+                textAlign="left"
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
