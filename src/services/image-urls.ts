c0d5d0e308af740fd5a6c2b511bd79fda8271d3b
ck496 <chriskurian24@gmail.com>
2025-06-20 01:05:05 -0700

    bug-fix: Temp fix for images no image url error

--- src/services/image-urls.ts
+++ src/services/image-urls.ts
@@ -2,6 +2,7 @@
 //      - The Url attribute for games returns full sized images which are not optimized for the small game cards
 //      - Since RAWG API supports cropping on the fly, create cropped urls to get smaller images for faster loads
 const getCroppedUrl = (url: string) => {
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const croppedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
